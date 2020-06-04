bcffdd55d9e6900c2ef61e6f2754b7f734fa8581
thiagobarros19 <thiago.rck00@gmail.com>
2020-06-03 21:50:35 -0300

    fix css

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,40 +1,12 @@
-
 import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import { IconButton} from '@material-ui/core';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ReorderIcon from '@material-ui/icons/Reorder';
 import { Link } from 'react-router-dom';
+import useStyles from './styles';
 
-const useStyles = makeStyles(theme => ({
-  list: {
-    minWidth: 200,
-    backgroundColor: fade(theme.palette.common.black, 0),
-   // marginTop:50,
-  },
-  listItem:{
-    '&:hover':{
-        backgroundColor: fade(theme.palette.common.black, 0.9),
-        color:fade(theme.palette.common.white, 1)
-        }
-  },
-  item:{
-      fontSize:20,
-      color:"#FFF",
-      textDecoration:"none"
-  },
-  fullList: {
-    width: 'auto',
-  },
-  drawer:{
-      '& > div':{
-        background:fade(theme.palette.common.black, 0.6),
-        color:fade(theme.palette.common.white, 0.8)
-      }
-  }
-}));
 
 export default function MyDrawer() {
   const classes = useStyles();
---
+++ src/components/Drawer/styles.js
@@ -0,0 +1,31 @@
+import { fade, makeStyles } from '@material-ui/core/styles';
+
+const styles = makeStyles(theme => ({
+    list: {
+      minWidth: 200,
+      backgroundColor: fade(theme.palette.common.black, 0),
+     // marginTop:50,
+    },
+    listItem:{
+      '&:hover':{
+          backgroundColor: fade(theme.palette.common.black, 0.9),
+          color:fade(theme.palette.common.white, 1)
+          }
+    },
+    item:{
+        fontSize:20,
+        color:"#FFF",
+        textDecoration:"none"
+    },
+    fullList: {
+      width: 'auto',
+    },
+    drawer:{
+        '& > div':{
+          background:fade(theme.palette.common.black, 0.6),
+          color:fade(theme.palette.common.white, 0.8)
+        }
+    }
+  }));
+
+  export default styles;
